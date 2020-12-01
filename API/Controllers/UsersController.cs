3ced4b41ea5345568f53750f6e68d36132838590
Michelle <michellevalizado@yahoo.com>
2020-12-01 16:49:23 -0600

    Initial commit with basic api controller

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<AppUser>> GetUsers(int id) 
+        {
+            var users = await _context.Users.FindAsync(id);
+            return users;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() 
+        {
+            var users = await _context.Users.ToListAsync();
+            return users;
+        }
+        
+    }
+}
